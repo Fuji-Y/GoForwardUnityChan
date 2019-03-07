0d5c7cef7f1944507e2bbc2f7011c8dd2c4384ec
Fuji-Y <ukiuki0221kari@gmail.com>
2019-03-07 18:05:25 +0900

    Lesson7 GoFowardUnityChan 提出
    メンタリング時のアドバイスと質問をもとに作成。

---
+++ Assets/BlockBGM.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBGM : MonoBehaviour
+{
+    //blockのBGM
+    AudioSource blockaudio;
+
+    //blockのBGMのON/OFF
+    private bool onoff = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.blockaudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "BlockTag" || collision.gameObject.tag == "GroundTag") //新しくTagをblockとgroundにつけた
+        {
+            this.onoff = true;
+        }
+
+        if (this.onoff == true)
+        {
+            blockaudio.Play();
+        }
+        this.onoff = false;
+    }
+}
+
+/*
+ジャンプ状態のときにはボリュームを0にする（追加）
+GetComponent<AudioSource>().volume = (isGround)? 1 : 0;
+*/
+
